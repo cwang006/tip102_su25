07cb143452b1aebaa5e89419b892bc253a104c09
cwang006 <cwang006.ccny@gmail.com>
2025-06-16 02:24:01 -0400

    Session 1, Problems 4-6

--- unit1_s1s1.py
+++ unit1_s1s1.py
@@ -1,15 +1,17 @@
+# Problem 1: Batman
 def batman():
     print("I am vengenance. I am the night. I am Batman!")
 # batman()
 
+# Problem 2: Mad Libs
 def madlib(verb):
     print(f"I have one power. I never {verb}. - Batman")
 verb = "give up"
 # madlib(verb)
-
 verb = "nap"
 # madlib(verb)
 
+# Problem 3: Trilogy
 def trilogy(year):
     if year == 2005:
         print("Batman Begins")
@@ -21,7 +23,59 @@ def trilogy(year):
         print(f"Christopher Nolan did not release a Batman movie ins {year}.")
 
 year = 2008
-trilogy(year)
-
+# trilogy(year)
 year = 1998
-trilogy(year)
+# trilogy(year)
+
+# Problem 4: Last
+def get_last(items):
+    if not items:
+        return None
+    else:
+        return items[-1:]
+        
+items = ["spider man", "batman", "superman", "iron man", "wonder woman", "black adam"]
+# print(get_last(items))
+items = []
+# print(get_last(items))
+
+# Problem 5: Concatenate
+def concatenate(words):
+    new_str = ''.join(words)
+    return new_str
+
+words = ["vengeance", "darkness", "batman"]
+# print(concatenate(words))
+
+words = []
+# print(concatenate(words))
+
+# Using an accumulator variable:
+def concatenate1(words):
+    result = ""
+    for word in words:
+        result += word
+    return result
+words = ["vengeance", "darkness", "batman"]
+# print(concatenate1(words))
+
+words = []
+# print(concatenate1(words))
+
+# Problem 6: Squared
+def squared(numbers):
+    result = []
+    for i in numbers:
+        sq_nums = i*i
+        result.append(sq_nums)
+    return result
+
+numbers = [1, 2, 3]
+# print(squared(numbers))
+
+# Another way:
+def squared1(numbers):
+    for i in numbers:
+        return i ** 2
+numbers = [1, 2, 3]
+# print(squared(numbers))
