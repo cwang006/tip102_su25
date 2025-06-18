763dad1711f198ac7f82cb10f383bd7043149477
cwang006 <cwang006.ccny@gmail.com>
2025-06-18 15:14:36 -0400

    Week 3, Session 1

--- Week 3/unit3_s1.py
+++ Week 3/unit3_s1.py
@@ -11,6 +11,8 @@ def is_valid_post_format(posts):
         if i in formats: 
             stack.append(i)
         elif i in formats.values():
+            if not stack:        # To account for an empty stack but more values
+                return False
             top = stack.pop()
             if formats[top] != i:
                 return False
@@ -22,13 +24,38 @@ def is_valid_post_format(posts):
 #print(is_valid_post_format("()"))
 #print(is_valid_post_format("()[]{}")) 
 #print(is_valid_post_format("(]"))
+#print(is_valid_post_format("())"))
 
 # Problem 2: Reverse User Comments Queue
 def reverse_comments_queue(comments):
-  comments = []
-  
-  for i in range(len(comments)-1):
-      comments.append(i)
-      
-print(reverse_comments_queue(["Great post!", "Love it!", "Thanks for sharing."]))
-print(reverse_comments_queue(["First!", "Interesting read.", "Well written."]))
+  stack = []
+  for i in range(len(comments)):
+      stack.append(comments[i])
+  reverse = []
+  while stack:
+      reverse.append(stack.pop())
+  return reverse
+    
+# print(reverse_comments_queue(["Great post!", "Love it!", "Thanks for sharing."]))
+# print(reverse_comments_queue(["First!", "Interesting read.", "Well written."]))
+
+# Problem 3: Check Symmetry in Post Titles
+def is_symmetrical_title(title):
+    left = 0
+    right = len(title)-1
+    regex = "(){}!@#$%^&*<>,.?;:' "
+    
+    while left < right:
+        if title[left] or title[right] in regex:
+            left += 1
+            right -= 1
+        if title[left].lower() == title[right].lower():
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
+
+
+print(is_symmetrical_title("A Santa at NASA"))
+print(is_symmetrical_title("Social Media"))
--- Week 3/w3s1_notes.py
+++ Week 3/w3s1_notes.py
@@ -1,1 +1,27 @@
-# Stacks are implemented using Python's list and 
+# Stacks use append() and pop()
+# Queues use append()/appendleft() and popleft()/pop()
+# Deque is like a double-ended stack? 
+
+# Instructor Demo:
+# Write a function that checks if a given string containing parentheses is balanced.
+# The function should return True if everyone opening parenthesis has a corresponding
+# parenthesis in the correct order, and False otherwise
+
+# The idea is every opening parenthesis is added to the stack, which allows us to keep
+# track of the total opening parenthesis
+
+def is_balanced(s):
+    open_parens = []
+    for c in s:
+        if c == "(":
+            open_parens.append(c)
+        else:
+            if open_parens:
+                open_parens.pop()
+            else:
+                return False
+    return not open_parens
+
+print(is_balanced("()"))
+print(is_balanced("())"))
+print(is_balanced("(()"))
